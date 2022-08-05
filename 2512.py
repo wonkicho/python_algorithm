eeddd3dced58ed4d56068784589c300bb8554762
wonkicho <chowk1109@naver.com>
2022-08-05 22:41:50 +0900

    solved 2512.py

---
+++ 2512.py
@@ -0,0 +1,21 @@
+# n = int(input())
+# money = list(map(int, input().split()))
+# tight = int(input())
+
+
+# avg_val = tight // n
+# over_idx = [money.index(m) for m in money if m > avg_val]
+
+
+# while True:
+#     if sum(money) <= tight:
+#         money_result = [money[idx] for idx in over_idx]
+#         print(min(money_result))
+#         break
+#     else:
+#         max_idx = money.index(max(money))
+#         money[max_idx] = max(money) - 1
+                
+    
+        
+        
