e549558b096635c617afc7a4f3b19b1ef44e820e
Wonki CHO <53808221+wonkicho@users.noreply.github.com>
2022-08-05 15:02:12 +0900

    solved 2864.py

---
+++ 2864.py
@@ -0,0 +1,18 @@
+n,m = input().split()
+n,m = str(n), str(m)
+
+min1 = n.replace('6','5')
+max1 = n.replace('5','6')
+
+min2 = m.replace('6','5')
+max2 = m.replace('5','6')
+
+
+
+
+
+print(int(min1)+int(min2), int(max1)+int(max2))
+
+
+
+    
