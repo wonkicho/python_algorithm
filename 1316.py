da380bf884f27aaa26abd186148d9968a3d69e7f
wonkicho <chowk1109@naver.com>
2022-08-08 13:05:31 +0900

    solved 1316.py

---
+++ 1316.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+result = 0
+for _ in range(n):
+    word = input()
+    error = 0
+    
+    
+    for i in range(len(word)-1):
+        if word[i] != word[i+1]:
+            new_word = word[i+1:]
+            
+            if new_word.count(word[i]) > 0:
+                error += 1
+    
+    if error == 0:
+        result += 1          
+        
+                
+print(result)
+    
